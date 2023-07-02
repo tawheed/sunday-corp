d615b616b1412b852589eb6649f28a0418324c56
TK <tawheed@gmail.com>
2023-07-02 02:29:08 -0500

    Styling FAQ and Resources section

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -1,38 +1,126 @@
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+
 export default function Faqs() {
+  const faq = [
+    {
+      question:
+        "What is the difference between a founder and a high performance founder?",
+      answer: "Sure thing",
+    },
+  ];
+
+  const videos = [
+    {
+      title: "11 Habits Of High Performance & Successful CEOs",
+      description:
+        "In this video, I share the 11 habits that I've seen successful CEOs use and that I've used myself to build successful companies. These habits are the foundation of high performance and will help you build a successful company.",
+      url: "https://www.youtube.com/watch?v=LFaFvIaIaBs",
+    },
+    {
+      title: "How to Get More Done as a Founder/CEO",
+      description:
+        "How do you actually get more done as a Startup CEO? In this episode, we dig into the 3 principles that I follow to get more done, stay sane, and avoid burnout so that I can build successful companies and be an effective startup leader.",
+      url: "https://www.youtube.com/watch?v=kX8uNDkNA9A",
+    },
+    {
+      title: "6 Skills for Successful Startup Founders:",
+      description:
+        "I asked myself, 'Who would I have to become to accomplish my goals?' I came up with a list of 6 key skills, which I've honed over time to unlock my full potential as a startup founder and accomplish some of my most outrageous goals.",
+      url: "https://www.youtube.com/watch?v=CaH1UHGB_yA",
+    },
+    {
+      title: "SaaS Founder Routines That Boost Productivity",
+      description:
+        "There's this one secret weapon in startups that none of us talk about enough and that's the idea of stamina. There are those that learn to master it and they can go the distance and there are those that don't.",
+      url: "https://www.youtube.com/watch?v=CaH1UHGB_yA",
+    },
+    {
+      title: "How I Use the Holiday Period to Plan My Life",
+      description:
+        "In this episode, I share how I use the holiday period to plan my life and my business. The holidays tend to be a tough time for Founders but it doesn't have to be when you have the right framework to follow.",
+      url: "https://www.youtube.com/watch?v=mhcA9AaMaVY",
+    },
+    {
+      title: "How to Scale Your Startup with an Unstoppable GTM Strategy",
+      description:
+        "I believe that you should have an Unstoppable strategy both for your personal life and for your business. In this episode, I share the 6 key steps I've consistently followed to build an unstoppable GTM Strategy.",
+      url: "https://www.youtube.com/watch?v=39Qkonv_JRs",
+    }
+  ];
+
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
           {/* Section header */}
-          <div className="pb-12 md:pb-20">
-            <h2 className="h2 font-hkgrotesk">FAQs</h2>
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 font-hkgrotesk mb-4">
+              What is Unstoppable?
+            </h2>
+            <p className="text-xl text-slate-500">
+              Every Founder needs a strategy for their personal life and their business. When you're with us, yours will be unstoppable.
+            </p>
           </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 mb-16">
+            {videos.map((video) => (
+              <div className="flex justify-center">
+                <div className="rounded-lg shadow-lg bg-slate-800">
+                  <div className="w-full rounded-t-lg h-[200px] bg-slate-800 pt-2">
+                    <ReactPlayer url={video.url} width="100%" height="100%" />
+                  </div>
+                  <div className="p-6">
+                    <h5 className="font-hkgrotesk font-bold text-white text-lg font-bold mb-2">
+                      {video.title}
+                    </h5>
+                    <p className="text-slate-500 text-base font-medium mb-4 leading-6">
+                      {video.description}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
           {/* Columns */}
           <div className="md:flex md:space-x-12 space-y-8 md:space-y-0">
             {/* Column */}
             <div className="w-full md:w-1/2 space-y-8">
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-hkgrotesk font-medium">What is the difference between the Free and Paid versions?</h4>
+                <h4 className="text-xl font-hkgrotesk font-medium">
+                  What is the difference between the Free and Paid versions?
+                </h4>
                 <p className="text-slate-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim
-                  ad minim veniam.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                  ut enim ad minim veniam.
                 </p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-hkgrotesk font-medium">Do you have student or non-profit discounts?</h4>
+                <h4 className="text-xl font-hkgrotesk font-medium">
+                  Do you have student or non-profit discounts?
+                </h4>
                 <p className="text-slate-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim
-                  ad minim veniam.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                  ut enim ad minim veniam.
                 </p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-hkgrotesk font-medium">How is the price determined?</h4>
+                <h4 className="text-xl font-hkgrotesk font-medium">
+                  How is the price determined?
+                </h4>
                 <p className="text-slate-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim
-                  ad minim veniam.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                  ut enim ad minim veniam.
                 </p>
               </div>
             </div>
@@ -40,26 +128,35 @@ export default function Faqs() {
             <div className="w-full md:w-1/2 space-y-8">
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-hkgrotesk font-medium">Do I need coding knowledge to use this product?</h4>
+                <h4 className="text-xl font-hkgrotesk font-medium">
+                  Do I need coding knowledge to use this product?
+                </h4>
                 <p className="text-slate-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim
-                  ad minim veniam.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                  ut enim ad minim veniam.
                 </p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-hkgrotesk font-medium">Is there a way to become an Affiliate reseller?</h4>
+                <h4 className="text-xl font-hkgrotesk font-medium">
+                  Is there a way to become an Affiliate reseller?
+                </h4>
                 <p className="text-slate-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim
-                  ad minim veniam.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                  ut enim ad minim veniam.
                 </p>
               </div>
               {/* Item */}
               <div className="space-y-2">
-                <h4 className="text-xl font-hkgrotesk font-medium">What forms of payment do you accept?</h4>
+                <h4 className="text-xl font-hkgrotesk font-medium">
+                  What forms of payment do you accept?
+                </h4>
                 <p className="text-slate-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua, ut enim
-                  ad minim veniam.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua,
+                  ut enim ad minim veniam.
                 </p>
               </div>
             </div>
@@ -67,5 +164,5 @@ export default function Faqs() {
         </div>
       </div>
     </section>
-  )
+  );
 }
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -182,15 +182,36 @@ export default function Footer() {
             <li>
               <a
                 className="flex justify-center items-center text-indigo-500 hover:text-slate-300 transition duration-150 ease-in-out"
-                href="#0"
+                href="https://twitter.com/tawheed"
                 aria-label="Twitter"
+                target="_blank"
               >
                 <svg
-                  className="w-8 h-8 fill-current"
-                  viewBox="0 0 32 32"
                   xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="w-8 h-8 fill-current"
                 >
-                  <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
+                  <path d="M23.94 4.68c-.88.39-1.81.65-2.78.77a4.9 4.9 0 0 0 2.15-2.7c-.95.56-2 .96-3.1 1.18a4.9 4.9 0 0 0-8.35 4.47A13.9 13.9 0 0 1 2.02 3.4a4.9 4.9 0 0 0 1.5 6.53A4.86 4.86 0 0 1 1.99 9v.06c0 2.17 1.55 3.98 3.6 4.4a4.9 4.9 0 0 1-2.2.08c.62 1.92 2.42 3.32 4.55 3.36A9.83 9.83 0 0 1 0 20.05a13.84 13.84 0 0 0 7.53 2.21c9 0 13.92-7.48 13.92-13.93 0-.21 0-.41-.01-.62.95-.69 1.79-1.56 2.45-2.55l-.03-.02z" />
+                </svg>
+              </a>
+            </li>
+            <li className="ml-2">
+              <a
+                className="flex justify-center items-center text-indigo-500 hover:text-slate-300 transition duration-150 ease-in-out"
+                href="https://getunstoppable.com/youtube"
+                target="_blank"
+                aria-label="Youtube"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  className="w-8 h-8 fill-current"
+                >
+                  <path d="M21.8,7.6c-0.2-0.7-0.7-1.3-1.4-1.5C18.7,4,12,4,12,4S5.3,4,3.6,6.1C2.9,6.3,2.4,6.9,2.2,7.6C2,8.7,2,12,2,12s0,3.3,0.4,4.4c0.2,0.7,0.7,1.3,1.4,1.5C5.3,20,12,20,12,20s6.7,0,8.4-1.1c0.7-0.2,1.2-0.8,1.4-1.5c0.4-1.1,0.4-4.4,0.4-4.4s0-3.3-0.4-4.4z M9,15.2V8.8l6.2,3.2L9,15.2z" />
                 </svg>
               </a>
             </li>
@@ -198,29 +219,16 @@ export default function Footer() {
               <a
                 className="flex justify-center items-center text-indigo-500 hover:text-slate-300 transition duration-150 ease-in-out"
                 href="#0"
-                aria-label="Medium"
+                aria-label="LinkedIn"
               >
                 <svg
-                  className="w-8 h-8 fill-current"
-                  viewBox="0 0 32 32"
                   xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  class="w-8 h-8 fill-current"
                 >
-                  <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z" />
-                </svg>
-              </a>
-            </li>
-            <li className="ml-2">
-              <a
-                className="flex justify-center items-center text-indigo-500 hover:text-slate-300 transition duration-150 ease-in-out"
-                href="#0"
-                aria-label="Github"
-              >
-                <svg
-                  className="w-8 h-8 fill-current"
-                  viewBox="0 0 32 32"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z" />
+                  <path d="M20.7,3H3.3C2.6,3,2,3.6,2,4.3v15.3C2,20.4,2.6,21,3.3,21h17.3c0.7,0,1.3-0.6,1.3-1.3V4.3C22,3.6,21.4,3,20.7,3z M8.6,18.5H5.8V9.5h2.8V18.5z M7.2,8.2c-0.9,0-1.6-0.7-1.6-1.6c0-0.9,0.7-1.6,1.6-1.6c0.9,0,1.6,0.7,1.6,1.6C8.8,7.5,8.1,8.2,7.2,8.2z M18.7,18.5h-2.8v-4.4c0-1.1-0.4-1.9-1.4-1.9c-0.7,0-1.1,0.5-1.4,1.1c0,0.1,0,0.2,0,0.3v4.9h-2.8v-9h2.6v1.2c0.4-0.6,1-1.1,2.1-1.1c1.5,0,2.6,1.2,2.6,3.1V18.5z" />
                 </svg>
               </a>
             </li>
@@ -229,14 +237,14 @@ export default function Footer() {
           <div className="text-sm text-slate-600">
             <a
               className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
-              href="#0"
+              href="https://tkkader.com/terms"
             >
               Terms
             </a>{" "}
             ·{" "}
             <a
               className="text-slate-500 hover:text-slate-300 transition duration-150 ease-in-out"
-              href="#0"
+              href="https://tkkader.com/privacy"
             >
               Privacy Policy
             </a>
