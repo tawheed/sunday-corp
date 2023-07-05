01143460a09435942edfc2210fb4896af176f8a6
TK <tawheed@gmail.com>
2023-07-04 19:43:45 -0500

    Returning empty

--- components/tracking.tsx
+++ components/tracking.tsx
@@ -1,9 +1,17 @@
 "use client";
 
 export default function Tracking({ event = "View Sales Page" }) {
-  typeof window !== "undefined" &&
-    window.mixpanel &&
-    window.mixpanel.track(event);
+  setTimeout(() => {
+  if(typeof window !== "undefined" &&
+    window.mixpanel) {
+      console.log("Logging Mixpanel Event: ", event);
+      window.mixpanel.track(event);
+    }
+    else {
+      console.log("Mixpanel not loaded");
+    }
+  }, 1000);
+
 
   return <></>;
 }
