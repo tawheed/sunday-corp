d01581705d0041cd033b9257edc3dbd2b32aa230
TK <tawheed@gmail.com>
2023-07-04 19:28:39 -0500

    Returning empty

--- components/tracking.tsx
+++ components/tracking.tsx
@@ -1,4 +1,8 @@
 "use client";
 export default function Tracking({ event }) {
     typeof window !== 'undefined' && window.mixpanel && window.mixpanel.track(event);
+
+    return (
+        <></>
+    )
 }
